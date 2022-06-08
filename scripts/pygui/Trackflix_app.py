bb410db1f17947573ca3bb2f017a735588833f87
Charles Shadrack <kooganshadrac@gmail.com>
2022-06-08 10:11:10 +0200

    prototype final

--- scripts/pygui/Trackflix_app.py
+++ scripts/pygui/Trackflix_app.py
@@ -1,15 +1,113 @@
 import dearpygui.dearpygui as dpg
+import pandas as pd
+import numpy as np
+import os
+import shutil
+from datetime import date
+import csv
+import time
+import zipfile
+import requests
+import steam
+from steam.steamid import SteamID
+from steam.webapi import WebAPI, get
+import steamleaderboards as sl
+import os,sys,stat
+from zipfile import ZipFile, ZipInfo
+import difflib
+import re
+import matplotlib.pyplot as plt
+import seaborn as sns
+from streamlit import stop
+from functions import find_folder, search_hs_scenario, hs_timer, find_closest_match, make_graphs_png
+from math import sin
+import sys
 
 dpg.create_context()
 
-width, height, channels, data = dpg.load_image("F:\coding\Bootcamp\10\scripts\pygui\Trackflix_logo.png")
+def dir_callback(sender, app_data):
 
-with dpg.texture_registry():
-    texture_id = dpg.add_static_texture(width, height, data)
+    global dataDir
 
-with dpg.window(label="Tutorial"):
-    dpg.add_image(texture_id)
+    dataDir = str((app_data.pop('file_path_name')))[1:]
+    
+    try:
+        print('finding folder...')
+        find_folder(dataDir)
+        print('done')
+    except Exception:
+        print('Error')
 
+def routine_callback(sender,app_data):
+
+    global closest_match
+    global stats
+    global scenario_names
+    global temp_df
+    global high_score_df
+    global current_hs
+
+    if app_data:  
+        print(f'user input: {app_data}')
+
+        user_input = app_data
+        stats = pd.read_csv('data/stats.csv')
+        scenario_names = (stats['Scenario'].unique()).astype(str)
+        closest_match = find_closest_match(stats,user_input)
+
+        print(f'retrieving data for routine: {closest_match}')
+        
+        temp_df = stats.loc[stats['Scenario']==closest_match]
+
+        search_hs_scenario(closest_match, stats, dataDir)
+
+        high_score_df = pd.read_csv('data/high_score_df.csv')
+        current_hs = max(high_score_df['Kill #'])
+
+        print('routine retrieved')
+        print(f'current high score :{current_hs}')
+    else:
+        print('no app_data')
+
+def graph_callback(sender,app_data):
+    if sender and closest_match:
+        progress_graphs = make_graphs_png(temp_df,closest_match)
+        with dpg.window(label="Progress Graphs", autosize=True):
+            dpg.add_image('data/progress_graph.png',width=100,height=100)
+            dpg.add_image('data/avg_progress_graph.png',width=100,height=100)
+
+def timer_callback(sender,app_data):
+    if sender:
+        hs_timer(high_score_df)
+
+def update_callback(sender,app_data):
+    if sender:
+        try:
+            print('finding folder...')
+            find_folder(dataDir)
+            search_hs_scenario(closest_match, stats, dataDir)
+            print('folder found')
+            print(f'curent high score: {current_hs}')
+        except Exception:
+            print('Error')
+    
+dpg.add_file_dialog(show=False, callback=dir_callback, tag="file_dialog_id", default_filename= '')
+dpg.create_viewport(title='Trackflix', width=800, height=500)
+with dpg.window(label="Directory and Routine",autosize=True,tag='Primary Window'):
+    dpg.add_text('PLEASE COMPLETE FIELDS IN ORDER!')
+    dpg.add_spacer(height=10)
+    dpg.add_text('When locating folder please click \'Drive\' and enter the FULL directory path into \'File Name\'')
+    dpg.add_button(label="Locate stats folder", callback=lambda: dpg.show_item("file_dialog_id"))
+    dpg.add_spacer(height=10)
+    dpg.add_text('Type your desired routine and hit enter')
+    dpg.add_input_text(label="Routine",callback=routine_callback,on_enter=True)
+    dpg.add_spacer(height=10)
+    dpg.add_button(label='See progress graphs',callback=graph_callback)
+    dpg.add_spacer(height=10)
+    dpg.add_button(label='Start timer',callback=timer_callback)
+    dpg.add_spacer(height=10)
+    dpg.add_button(label='Manual retrieve routine latest score',callback=update_callback)
+dpg.setup_dearpygui()
+dpg.show_viewport()
 dpg.start_dearpygui()
-
 dpg.destroy_context()
--- scripts/pygui/functions.py
+++ scripts/pygui/functions.py
@@ -1,61 +1,232 @@
-#no need to change anything in this file!
-
-#SMS Spam Filter Imports
-import random
 import pandas as pd
-import string
-import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
+import numpy as np
+import os
+import shutil
+from datetime import date
+import csv
+import time
+import zipfile
+import requests
+import steam
+from steam.steamid import SteamID
+from steam.webapi import WebAPI, get
+import steamleaderboards as sl
+import os,sys,stat
+from zipfile import ZipFile, ZipInfo
+import difflib
+import re
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-def categorize_words():
-    '''
-    Catagorizes each spam/non-spam word into a corresponding list
-    Repeating words in each list will help with categorizing
-    '''
-    spam_words = []
-    ham_words = []
-    for sms in data['processed'][data['label'] == 'spam']:
-        for word in sms:
-            spam_words.append(word)
-    for sms in data['processed'][data['label'] == 'ham']:
-        for word in sms:
-            ham_words.append(word)
-    return spam_words, ham_words
+def find_folder(dataDir):
+  #dataDir = 'G:\Steam\steamapps\common\FPSAimTrainer\FPSAimTrainer\stats'
+  target = ['Kills:', 'Avg TTK:', 'Score:', 'Scenario:','Damage Done:']
 
-def predict(user_input):
-  spam_counter = 0
-  ham_counter = 0
+  try:
+      stats = pd.read_csv(dataDir + '\\stats.csv')
+  except:
+      stats = pd.DataFrame(columns=['Kills', 'Avg TTK', 'Score', 'Scenario','Damage Done'])
 
-  #add text colour : ham is green, spam is red
-  red = [220,50,50]
-  green = [100,220,50]
+  # Empty dict to store our csv data
+  data = {}
 
-  for word in user_input:
-    spam_counter += spam_words.count(word)
-    ham_counter += ham_words.count(word)
+  # Go through all the files and store our target data in our empty dictionary, including the datetime of the file.
+  for filename in os.listdir(dataDir):
 
-  if ham_counter > spam_counter:
-    #adding accuracy
-    certainty = round((ham_counter / (ham_counter + spam_counter)) * 100, 2)
-    return 'message is not spam, with {}% certainty'.format(certainty), green
-  elif spam_counter > ham_counter:
-    certainty = round((spam_counter / (ham_counter + spam_counter)) * 100, 2)
-    return 'message is spam, with {}% certainty'.format(certainty), red
-  else:
-    return 'message could be spam, with 50% certainty', [255,255,255]
+      f = open(f'{dataDir}/{filename}', 'r')
+      with f as csv_file:
+          csv_reader = csv.reader(csv_file)
+          for line in csv_reader:
+              try:
+                  if line[0] in target:
+                      data[(line[0][:-1])] = line[1]     
+              except IndexError:
+                  continue
+          f.close()
 
-def pre_process(sms):
-    '''
-    Remove punctuation and stop words from the custom sms
-    '''
-    remove_punct = "".join([word.lower() for word in sms if word not in string.punctuation])
-    tokenize = nltk.tokenize.word_tokenize(remove_punct)
-    remove_stop_words = [word for word in tokenize if word not in nltk.corpus.stopwords.words('english')]
-    return remove_stop_words
+      scenario_date = f'{dataDir}/{filename}'[-29:-10]
+      data["Date"] = scenario_date
+      df = pd.DataFrame(data, index=[0])
+      stats = pd.concat([stats,df]).drop_duplicates().reset_index(drop=True)
 
-data = pd.read_csv('SMSSpamCollection.txt', sep = '\t', header=None, names=["label", "sms"])
-data['processed'] = data['sms'].apply(lambda x: pre_process(x))
+  stats['Kills'] = pd.to_numeric(stats['Kills'], downcast='integer')
+  stats['Avg TTK'] = pd.to_numeric(stats['Avg TTK'], downcast='float')
+  stats['Score'] = pd.to_numeric(stats['Score'], downcast='float')
+  stats['Date'] = pd.to_datetime(stats['Date'], yearfirst=True, format=('%Y.%m.%d-%H.%M.%S'),exact=False)
 
-#creating lists to store spam/non-spam associated words and their instances
-spam_words, ham_words = categorize_words()
+  stats.to_csv("data/stats.csv", index=False)
+#find and clean data
+
+def find_closest_match(stats,user_input):
+
+  scenario_names = (stats['Scenario'].unique()).astype(str)
+  try:
+      closest_match = (difflib.get_close_matches(str(user_input), scenario_names, n=1,cutoff=0.45).pop(0))
+      return closest_match  
+  except Exception:
+      print('we can\'t find that scenario. Did u spel it rite?')
+#find the closest match to the user input routine
+
+def search_hs_scenario(closest_match, stats, dataDir):
+
+  #scenario_names = (stats['Scenario'].unique()).astype(str)
+
+  #try:
+  #    closest_match = (difflib.get_close_matches(str(user_input), scenario_names, n=1,cutoff=0.45).pop(0))
+  #    print(closest_match)
+  #except Exception:
+  #    print('we can\'t find that scenario. Did u spel it rite?')
+  #
+  scenario = stats.loc[stats['Scenario'] == closest_match]
+
+  high_score_date = []
+# The following is simply to catch the high_score_date, nothing more!
+  if sum(scenario['Score']) == 0.0:
+      high_score_date.append(scenario.sort_values('Kills')['Date'].iloc[-1])
+  elif sum(scenario['Kills']) > 0  and sum(scenario['Score']) > 0.0:
+      high_score_date.append(scenario.sort_values('Score')['Date'].iloc[-1])
+
+  hs_date = str(high_score_date[0])
+
+  hs_year = hs_date[:4]
+  hs_month = hs_date[5:7]
+  hs_day = hs_date[8:10]
+  hs_hour = hs_date[11:13]
+  hs_min = hs_date[14:16]
+  hs_sec = hs_date[17:19]
+
+  Kill = []
+  Time = []
+  Acc = []
+  Challenge_start = []
+
+  for filename in os.listdir(dataDir):
+      if f'{hs_year}.{hs_month}.{hs_day}-{hs_hour}.{hs_min}.{hs_sec} Stats.csv' in filename:
+          f = open(f'{dataDir}/{filename}', 'r')
+          with f as csv_file:
+              csv_reader = csv.reader(csv_file)
+              for line in csv_reader:
+                  try:
+                      Kill.append(line[0])
+                      Time.append(line[1])
+                      Acc.append(line[7])
+                  except Exception:
+                      break 
+              for line in csv_reader:
+                  try:
+                      if line[0] in 'Challenge Start:' and line[1] not in 'H:M:S.s':
+                          Challenge_start.append(line[1])
+                      elif line[0] in 'Challenge Start:' and line[1] in 'H:M:S.s' or '':
+                          start = pd.to_datetime(Time[-1]) - ((pd.to_datetime(Time[-1]) - pd.to_datetime(Time[1])).round(freq='T')) # deduce length between challenge start and first kill
+                          Challenge_start.append(str(start)[11:-3])
+                  except Exception:
+                      continue              
+          f.close()
+
+  data_tuples = list(zip(Kill,Time,Acc))
+
+  high_score_df = (pd.DataFrame(data_tuples,columns=('Kill #','Time', 'Accuracy'))
+                      .replace(['Kill #','Accuracy'], 0)
+                      ).replace('Timestamp',''.join(str(Challenge_start[0])))
+
+  high_score_df['Kill #'] = pd.to_numeric(high_score_df['Kill #'], downcast='integer')
+  high_score_df['Accuracy'] = pd.to_numeric(high_score_df['Accuracy'], downcast='float')
+  high_score_df['Time'] = pd.to_datetime(high_score_df['Time'],format=('%H:%M:%S.%f'),exact=False)
+  high_score_df['ms'] = (high_score_df['Time'] - high_score_df['Time'].iloc[0]) / pd.to_timedelta('1ms')
+
+  high_score_df.to_csv('data/high_score_df.csv',index=False)
+#search for your highest score
+
+def make_graphs_png(temp_df,closest_match):
+    #temp_df = stats.loc[stats['Scenario']==closest_match]
+
+    #GRAPH 1
+    plt.rcParams["figure.figsize"] = (20,10)
+    plt.xlabel('Date')
+    plt.ylabel('Score/Kills')
+    plt.title(f'{closest_match} Progress')
+    plt.xticks(rotation=90)
+    plt.grid(visible=True)
+    plt.plot(temp_df['Date'], temp_df['Score'], color='green', marker='o', linestyle='solid',
+         linewidth=2, markersize=2)
+    plt.savefig('data/progress_graph.png')
+    
+
+    #GRAPH 2
+    x = np.array(range(len(temp_df['Date']))).reshape((-1, 1))
+    y = np.array(temp_df['Score']).reshape((-1, 1))
+    plt.figure()
+    plt.xlim(min(x), max(x))
+    ax = sns.regplot(x=x,y=y)
+    plt.xlabel('Days since started routine')
+    plt.ylabel('Score/Kills')
+    plt.title(f'{closest_match} AVG Progress')
+    plt.grid(visible=True)
+    plt.savefig('data/avg_progress_graph.png')
+    
+    plt.show()
+
+    #TTK Graph
+    slowest_ttk = max(temp_df['Avg TTK'])
+
+    if slowest_ttk > 0:
+        plt.rcParams["figure.figsize"] = (20,12)
+        plt.xlabel('Date')
+        plt.ylabel('Average_TTK in seconds')
+        plt.title(f'{closest_match} AVG_TTK Progress')
+        plt.xticks(rotation=90)
+        plt.grid(visible=True)
+        plt.plot(temp_df['Date'], temp_df['Avg TTK'], color='blue', marker='o', linestyle='solid',
+            linewidth=2, markersize=2)
+        plt.savefig('data/progress_graph.png')
+        plt.show()
+    
+      
+# line and linear regression of your progress
+
+def hs_timer(high_score_df):
+    score_kills = 0
+
+    for index, value in enumerate(high_score_df['ms']):
+        if value == 0.0:
+            print('3',end='\r')
+            time.sleep(1.0)
+            print('2',end='\r')
+            time.sleep(1.0)
+            print('1',end='\r')
+            time.sleep(1.0)
+            print('GO',end='\r')      
+            time.sleep((high_score_df['ms'][1])/1000)
+            score_kills += 1
+            print(f'Score/Kills: {score_kills}',end='\r')
+        else:
+            try:
+                if score_kills<max(high_score_df['Kill #']):
+                    time.sleep((high_score_df['ms'][index+1] - high_score_df['ms'][index])/1000)
+                    score_kills += 1 
+                    print(f'Score/Kills: {score_kills}',end='\r')
+                else:
+                    print(f'finished! Current HighScore:{score_kills}',end='\r')
+            except Exception:
+                break
+# replays your high score kill number and counts
+# them up in realtime
+
+def progress_stats(temp_df,closest_match):
+    
+    max_score = max(temp_df['Score'])
+    lowest_score = min(temp_df['Score'])
+    score_diff = max_score-lowest_score
+    scor_percent_diff = round((score_diff/max_score)*100,2)
+    slowest_ttk = max(temp_df['Avg TTK'])
+    fastest_ttk = min(temp_df['Avg TTK'])
+    ttk_diff = slowest_ttk - fastest_ttk
+
+    if slowest_ttk > 0:
+        ttk_percent_diff = round((ttk_diff/slowest_ttk)*100,2)
+        print(f'{ttk_percent_diff}% or {ttk_diff} points improvement')
+    else:
+        print(f'Kovaaks did not provide any TTK information for this routine, try another')
+
+    print(f'{score_diff} points or {scor_percent_diff}% between your lowest and highest scores')
+# shows you your percentage improvement since beginning the routine
